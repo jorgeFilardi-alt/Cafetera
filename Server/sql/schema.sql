c5e4b7d5d1b565ef1969a654c760652828b3636b
brunofrancoucu <bruno.franco@correo.ucu.edu.uy>
2025-06-15 15:32:28 -0300

    add: execute the script multiple times without errors

--- Server/sql/schema.sql
+++ Server/sql/schema.sql
@@ -1,19 +1,19 @@
 -- Schema / Setup / Tablas
-CREATE DATABASE gestion_comercial;
+CREATE DATABASE IF NOT EXISTS gestion_comercial;
 USE gestion_comercial;
 
-CREATE TABLE login(
+CREATE TABLE IF NOT EXISTS login(
     correo VARCHAR(20) PRIMARY KEY,
     contraseña VARCHAR(100),
     es_administrador BOOLEAN,
     CHECK (LENGTH(contraseña) > 8)
 );
-CREATE TABLE proveedores(
+CREATE TABLE IF NOT EXISTS proveedores(
     id_proveedor INT PRIMARY KEY,
     nombre VARCHAR(20),
     telefono INT --Se reemplaza la columna sugerida "contacto" por "telefono" para evitar distintos tipos de dato de contacto
 );
-CREATE TABLE insumos(
+CREATE TABLE IF NOT EXISTS insumos(
     id_insumo INT PRIMARY KEY,
     descripcion VARCHAR(50),
     tipo VARCHAR(20),
@@ -21,14 +21,14 @@ CREATE TABLE insumos(
     id_proveedor INT,
     Foreign Key (id_proveedor) REFERENCES proveedores(id_proveedor)
 );
-CREATE TABLE clientes(
+CREATE TABLE IF NOT EXISTS clientes(
     id_cliente INT PRIMARY KEY,
     nombre VARCHAR(20),
     dirección VARCHAR(50),
     telefono INT,
     correo VARCHAR(50)
 );
-CREATE TABLE maquinas(
+CREATE TABLE IF NOT EXISTS maquinas(
     id_maquina INT PRIMARY KEY,
     modelo varchar(20),
     id_cliente INT NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE maquinas(
     costo_alquiler_mensual INT NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
-CREATE TABLE registro_consumo(
+CREATE TABLE IF NOT EXISTS registro_consumo(
     id_consumo INT PRIMARY KEY,
     id_maquina INT NOT NULL,
     id_insumo INT NOT NULL,
@@ -45,13 +45,13 @@ CREATE TABLE registro_consumo(
     Foreign Key (id_maquina) REFERENCES maquinas(id_maquina),
     Foreign Key (id_insumo) REFERENCES insumos(id_insumo)
 );
-CREATE TABLE tecnicos(
+CREATE TABLE IF NOT EXISTS tecnicos(
     ci INT PRIMARY KEY,
     nombre VARCHAR(20),
     apellido VARCHAR(20),
     telefono INT
 );
-CREATE TABLE mantenimientos(
+CREATE TABLE IF NOT EXISTS mantenimientos(
     id_mantenimiento INT PRIMARY KEY,
     id_maquina INT NOT NULL,
     ci_tecnico INT NOT NULL,
