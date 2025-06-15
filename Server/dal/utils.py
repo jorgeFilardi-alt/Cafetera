4262d44235275787d372421cf833785f8feaa2f0
brunofrancoucu <bruno.franco@correo.ucu.edu.uy>
2025-06-14 23:54:01 -0300

    add: basic queries endpoints

--- Server/dal/utils.py
+++ Server/dal/utils.py
@@ -40,3 +40,19 @@ def get_table(table: str):
     db_cursor(query)
     return results
 
+"""
+Funcion utilitaria, data point especifico (row / record / entry)
+Dado una tabla, propiedad, y valor
+Simplifica: endpoints individuales simples, ej. https://localhost:8000/cliente/123456
+"""
+def get_entry(table: str, property: str, value):
+    results = []
+    
+    def query(cursor):
+        query = f"SELECT * FROM gestion_comercial.{table} WHERE {property} = %s"
+        cursor.execute(query, (value,))
+        for row in cursor:
+            results.append(row)
+
+    db_cursor(query)
+    return results
--- Server/main.py
+++ Server/main.py
@@ -1,14 +1,31 @@
 """
-API server using FastAPI
+API server using FastAPI (Backend main entry point)
 
 uvicorn main:app --reload
 """
 from fastapi import FastAPI # pip install fastapi uvicorn
-from dal import get_clientes, get_tecnicos
+from dal.queries import get_clientes, get_tecnicos
+from dal.utils import get_table
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    # smpar = get_clientes("123456")
-    smpar = get_tecnicos("123456")  
-    return {"message": f"Hello World, {2, smpar}"}
+@app.get("/clientes")
+async def clientes():
+    return get_table("clientes")
+
+@app.get("/proveedores")
+async def proveedores():
+    return get_table("proveedores")
+
+@app.get("/tecnicos")
+async def tecnicos():
+    return get_table("tecnicos")
+
+@app.get("/insumos")
+async def insumos():
+    return get_table("insumos")
+
+# @app.get("/")
+# async def root():
+#     # smpar = get_clientes("123456")
+#     smpar = get_tecnicos("123456")  
+#     return {"message": f"Hello World, {2, smpar}"}
