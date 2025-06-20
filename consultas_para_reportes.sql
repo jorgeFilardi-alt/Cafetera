06f117e0693781edea8e8e4619b54251436d873b
Felipe Martiarena <femar1036@gmail.com>
2025-06-19 23:38:23 -0300

    Add files via upload

--- consultas_para_reportes.sql
+++ consultas_para_reportes.sql
@@ -1,8 +1,12 @@
-SELECT SUM(m.costo_alquiler_mensual + c.cantidad_usada*i.precio_unitario) AS total_a_cobrar
+USE Obligatorio;
+
+SELECT m.id_cliente, SUM(m.costo_alquiler_mensual + c.cantidad_usada*i.precio_unitario) AS total_a_cobrar
 FROM máquinas m
 JOIN registro_consumo c ON m.id_máquina = c.id_máquina
 JOIN insumos i ON c.id_insumo = i.id_insumo
-GROUP BY m.id_cliente;
+WHERE YEAR(c.fecha) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH) AND MONTH(c.fecha) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
+GROUP BY m.id_cliente
+ORDER BY m.id_cliente;
 
 SELECT i.id_insumo, i.precio_unitario, SUM(c.cantidad_usada) AS consumo
 FROM insumos i
