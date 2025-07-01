97b325dbddec2741bbf1dcd70ac477080aaeae23
brunofrancoucu <bruno.franco@correo.ucu.edu.uy>
2025-07-01 12:39:57 -0300

    refactor: main restructure readability

--- Server/main.py
+++ Server/main.py
@@ -1,28 +1,34 @@
 """
 API server - FastAPI (Backend entry point)
-
-uvicorn main:app --reload
+Estructura de api: `./Consigna.pdf`
 """
+from fastapi import FastAPI, Request # type: ignore
 
-from fastapi import FastAPI, Request # pip install fastapi uvicorn
-from dataclasses import dataclass, asdict
 from exceptions import InternalException
-# import dal.queries as queries # read?
-import dal.crud as crud
-import dal.auth as auth
-import sql.models as models
+import openapi as custom_openapi
 import middleware # mdlwr
-import helpers
-# from dal.utils import db_cursor
-import dal.utils as utils
-import openapi
+
+import sql.models as models # tipado del schema
+import dal.auth as auth # autenticacion
+import dal.crud as crud # SQL ops
 
 app = FastAPI()
-
 # /docs -H Authorization (button)
-app.openapi = lambda: openapi.custom(app)
+app.openapi = lambda: custom_openapi.custom(app)
 app.middleware("http")(middleware.access)
 app.middleware("http")(middleware.exceptions)
+
+# Autenticacion y usuarios
+
+@app.post("/login")
+async def login(req: models.Login):
+    payload = auth.gen_jwt(req.correo, req.pwd_hash)
+    return payload # Devolver token JWT
+
+@app.post("/register")
+async def register(_, entry: models.Login):
+    entry = models.Login(correo=entry.correo, pwd_hash=auth.get_creds("", entry.pwd_hash))
+    return crud.create("login", entry)
 
 # 1. ABM, Alta baja y modificacion
 
@@ -66,46 +72,38 @@ async def reporte_maquinas():
     """
     return crud.sql_file("queries/maquinas")
 
-# SQL Endpoints
+# SQL Table endpoints
 
 @app.get("/clientes")
-async def clientes(req: Request):
+async def clientes():
     return crud.get_table("clientes")
 
-@app.get("/cliente") # singular, ej. GET localhost:8000/cliente?id_cliente=201
-async def cliente(id_cliente: str = None, name: str = None, username: str = None):
-    return crud.get_entry("clientes", "id_cliente", id_cliente)
-
 @app.get("/proveedores")
-async def proveedores(req: Request):
+async def proveedores():
     return crud.get_table("proveedores")
 
-@app.get("/proveedor") # singular, ej. GET localhost:8000/proveedor?id_proveedor=101
-async def proveedor(id_proveedor: int = None, name: str = None, username: str = None):
-    return crud.get_entry("proveedores", "id_proveedor", id_proveedor)
-
 @app.get("/tecnicos")
-async def tecnicos(req: Request):
+async def tecnicos():
     return crud.get_table("tecnicos")
 
-@app.get("/tecnico") # singular, ej. GET localhost:8000/tecnico?ci=45556667
-async def tecnico(ci: str = None, name: str = None, username: str = None): # TODO: ...params
-    return crud.get_entry("tecnicos", "ci", ci)
-
 @app.get("/insumos")
-async def insumos(req: Request):
+async def insumos():
     return crud.get_table("insumos")
 
-@app.get("/insumo") # singular, ej. GET localhost:8000/insumo?id_insumo=1
-async def insumo(id_insumo: str = None, name: str = None, username: str = None):
-    return crud.get_entry("insumos", "id_insumo", id_insumo)
+# SQL Entry endpoints
 
-@app.post("/login")
-async def login(req: models.Login):
-    payload = auth.gen_jwt(req.correo, req.pwd_hash)
-    return payload # Devolver token JWT
+@app.get("/cliente/{uId}") # singular, ej. GET localhost:8000/cliente?id_cliente=201
+async def cliente(uId: int | str):
+    return crud.get_entry("clientes", "id_cliente", uId)
 
-@app.post("/register")
-async def register(req: Request, entry: models.Login):
-    entry = models.Login(correo=entry.correo, pwd_hash=auth.get_creds("", entry.pwd_hash))
-    return crud.create("login", entry)
+@app.get("/proveedor/{uId}") # singular, ej. GET localhost:8000/proveedor?id_proveedor=101
+async def proveedor(uId: int | str):
+    return crud.get_entry("proveedores", "id_proveedor", uId)
+
+@app.get("/tecnico/{uId}") # singular, ej. GET localhost:8000/tecnico?ci=45556667
+async def tecnico(uId: int | str):
+    return crud.get_entry("tecnicos", "ci", uId)
+
+@app.get("/insumo/{uId}") # singular, ej. GET localhost:8000/insumo?id_insumo=1
+async def insumo(uId: int | str):
+    return crud.get_entry("insumos", "id_insumo", uId)
