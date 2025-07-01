13cdc62de3d09c165dce50c0e4312b1fac8abd3b
brunofrancoucu <bruno.franco@correo.ucu.edu.uy>
2025-06-30 21:27:20 -0300

    refactor: openapi to module

--- Server/main.py
+++ Server/main.py
@@ -14,37 +14,12 @@ import sql.models as models
 import middleware # mdlwr
 import helpers
 from dal.utils import db_cursor
-from fastapi.security import OAuth2PasswordBearer
-from fastapi.openapi.utils import get_openapi
+import openapi
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="My API",
-        version="1.0.0",
-        description="API with Bearer Auth",
-        routes=app.routes,
-    )
-    openapi_schema["components"]["securitySchemes"] = {
-        "BearerAuth": {
-            "type": "http",
-            "scheme": "bearer",
-            "bearerFormat": "JWT",
-        }
-    }
-    for path in openapi_schema["paths"].values():
-        for operation in path.values():
-            operation["security"] = [{"BearerAuth": []}]
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-app.openapi = custom_openapi
-
+# /docs -H Authorization (button)
+app.openapi = lambda: openapi.custom(app)
 app.middleware("http")(middleware.access)
 app.middleware("http")(middleware.exceptions)
 
