4a207df34f0f514aea54a0e98ebfcf636ac1836e
brunofrancoucu <bruno.franco@correo.ucu.edu.uy>
2025-07-01 12:22:54 -0300

    add: queries reporte

--- Server/main.py
+++ Server/main.py
@@ -13,7 +13,8 @@ import dal.auth as auth
 import sql.models as models
 import middleware # mdlwr
 import helpers
-from dal.utils import db_cursor
+# from dal.utils import db_cursor
+import dal.utils as utils
 import openapi
 
 app = FastAPI()
@@ -22,6 +23,50 @@ app = FastAPI()
 app.openapi = lambda: openapi.custom(app)
 app.middleware("http")(middleware.access)
 app.middleware("http")(middleware.exceptions)
+
+# 1. ABM, Alta baja y modificacion
+
+@app.put("/proveedor") # UPDATE proveedor
+async def update_proveedor(req: Request, entry: models.Proveedor):
+    # Solo usuarios administradores
+    if not req.state.user.es_administrador:
+        raise InternalException("Operacion requiere rol de administrador.", 401, f"No admin en {req.path}, {entry}", "PUT:/proveedor")
+    
+    # Actualizar proveedor
+    return crud.update("proveedores", entry)
+
+# 5. Consultas para reportes
+
+@app.get("/reporte-clientes")
+async def reporte_clientes():
+    """
+    Reporte 5.1: total mensual a cobrar a cada cliente
+    suma alquileres de maquinas mas costo insumos cosnumidos
+    """
+    return crud.sql_file("queries/clientes")
+
+@app.get("/reporte-consumos")
+async def reporte_consumos():
+    """
+    Reporte 5.2: consumo de insumos con precio
+    """
+    return crud.sql_file("queries/consumos")
+
+@app.get("/reporte-mantenimientos")
+async def reporte_mantenimientos():
+    """
+    Reporte 5.3: tecnicos con mas mantenimientos
+    """
+    return crud.sql_file("queries/mantenimientos")
+
+@app.get("/reporte-maquinas")
+async def reporte_maquinas():
+    """
+    Reporte 5.4: clientes con mas maquinas
+    """
+    return crud.sql_file("queries/maquinas")
+
+# SQL Endpoints
 
 @app.get("/clientes")
 async def clientes(req: Request):
@@ -38,15 +83,6 @@ async def proveedores(req: Request):
 @app.get("/proveedor") # singular, ej. GET localhost:8000/proveedor?id_proveedor=101
 async def proveedor(id_proveedor: int = None, name: str = None, username: str = None):
     return crud.get_entry("proveedores", "id_proveedor", id_proveedor)
-
-@app.put("/proveedor") # UPDATE proveedor
-async def update_proveedor(req: Request, entry: models.Proveedor):
-    # Solo usuarios administradores
-    if not req.state.user.es_administrador:
-        raise InternalException("Operacion requiere rol de administrador.", 401, f"No admin en {req.path}, {entry}", "PUT:/proveedor")
-    
-    # Actualizar proveedor
-    return crud.update("proveedores", entry)
 
 @app.get("/tecnicos")
 async def tecnicos(req: Request):
@@ -73,8 +109,3 @@ async def login(req: models.Login):
 async def register(req: Request, entry: models.Login):
     entry = models.Login(correo=entry.correo, pwd_hash=auth.get_creds("", entry.pwd_hash))
     return crud.create("login", entry)
-
-@app.get("/reporte")
-async def reporte(req: Request):
-    results = []
-    return results
